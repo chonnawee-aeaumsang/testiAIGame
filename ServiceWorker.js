f037d9ce935f835ff2ea255eda2e5cdd85987cc0
chonnawee-aeaumsang <110365562+chonnawee-aeaumsang@users.noreply.github.com>
2024-10-29 16:19:10 +0700

    moreFiles

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "iAI-iAI Clicker-1.3.8";
+const contentToCache = [
+    "Build/i-AI-build.loader.js",
+    "Build/i-AI-build.framework.js",
+    "Build/i-AI-build.data",
+    "Build/i-AI-build.wasm",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
